0a1e7fb5074f9c5c20ce7b355ae61df22bf62c6d
vladimirArtsruni <65812947+vladimirArtsruni@users.noreply.github.com>
2021-02-15 15:46:41 +0400

    vladimir.artsruni
    asdasd

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -3,7 +3,11 @@ const categoryModel = require('../models/category'),
     subSubCategoryModel = require('../models/subSubCategory'),
     productsModel = require('../models/products');
 
+
 const LIMIT = 100;
+var fs = require('fs');
+const BunnyStorage = require('bunnycdn-storage').default;
+const bunnyStorage = new BunnyStorage('2a944ef0-da96-4a87-8201f411dcb2-5fc9-4073', 'fashinbutik/');
 
 class ApiController {
 
@@ -29,9 +33,9 @@ class ApiController {
         let page = req.query.page || 1;
         let products = {};
         let q = {
-            categoryID: { "$ne": null },
-            subCategoryID: { "$ne": null },
-            subSubCategoryID: { "$ne": null }
+            categoryID: {"$ne": null},
+            subCategoryID: {"$ne": null},
+            subSubCategoryID: {"$ne": null}
         };
 
         if (req.query.term) {
@@ -52,11 +56,57 @@ class ApiController {
 
     }
 
-    async productDelete(req, res){
+    async productDelete(req, res) {
         let {id} = req.params;
         await productsModel.deleteOne({_id: id});
-        res.json({ status: true, msg: "Album was deleted successfully"});
+        res.json({status: true, msg: "Album was deleted successfully"});
     }
+
+    async deleteCategory(req, res) {
+        let model = null;
+
+        if (req.body.type === 'category') {
+            model = await categoryModel.findById(req.params.id)
+        } else if (req.body.type === 'subCategory') {
+            model = await subCategoryModel.findById(req.params.id)
+        } else {
+            model = await subSubCategoryModel.findById(req.params.id)
+        }
+
+         console.log(model)
+        await model.delete()
+        res.json({status: true});
+
+    }
+
+    async updateImages(req, res) {
+        console.log(req.body)
+        // console.log(req.file)
+
+        let model = null;
+
+        if (req.body.type === 'category') {
+            model = await categoryModel.findById(req.body.id)
+        } else if (req.body.type === 'subCategory') {
+            model = await subCategoryModel.findById(req.body.id)
+        } else {
+            model = await subSubCategoryModel.findById(req.body.id)
+        }
+
+        let image = fs.readFileSync(req.file.path);
+        let key = `category/${req.body.type}/${req.file.filename}.${req.file.originalname.split('.')[req.file.originalname.split('.').length - 1]}`
+        await bunnyStorage.upload(image, key);
+
+
+        console.log(req.body.id)
+        console.log(req.body.type)
+        model.imageUrl = `${process.env.PULL_ZONE_URL}${key}`
+        await model.save();
+
+        res.json({status: `${process.env.PULL_ZONE_URL}${key}`});
+
+    }
+
 }
 
 
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+
+var upload = multer({ dest: 'temp/', })
+
 const apiController = require('../controllers/api.controller');
 
+router.post('/categories/update-image', upload.single('image'), apiController.updateImages);
 
 router.get('/categories', apiController.categories);
+router.delete('/categories/:id', apiController.deleteCategory);
+
 router.get('/products', apiController.products);
 router.post('/products/:id', apiController.productDelete);
 
